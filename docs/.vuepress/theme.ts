0da12403fa3494ef0e0b4cb5ab2f67e8dbfe5b36
White <294476387@qq.com>
2023-01-11 17:49:59 +0800

    add Waline

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -75,28 +75,13 @@ export default hopeTheme({
     // If you don't need comment feature, you can remove following option
     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-    // comment: {
-    /**
-     * Using Giscus
-     */
-    // provider: "Giscus",
-    // repo: "vuepress-theme-hope/giscus-discussions",
-    // repoId: "R_kgDOG_Pt2A",
-    // category: "Announcements",
-    // categoryId: "DIC_kwDOG_Pt2M4COD69",
-
-    /**
-     * Using Twikoo
-     */
-    // provider: "Twikoo",
-    // envId: "https://twikoo.ccknbc.vercel.app",
-
+    comment: {
     /**
      * Using Waline
      */
-    // provider: "Waline",
-    // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
-    // },
+    provider: "Waline",
+      serverURL: "https://comment.white233.top",
+    },
     blog: true,
     mdEnhance: {
       align: true,
