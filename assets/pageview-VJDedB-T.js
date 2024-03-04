86c52ad93bdf00989a9e08f4867cd4f4e753cc70
whiteChen233 <294476387@qq.com>
2024-03-04 05:17:51 +0000

    Deploying to gh-pages from @ whiteChen233/whitechen233.github.io@5931d7a138ab62853f1871e91a6c4fb5f697ea42 🚀

--- assets/pageview-VJDedB-T.js
+++
@@ -1,7 +0,0 @@
-import{a6 as r}from"./app-_iIN9QOS.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment.white233.top"};const i=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-_iIN9QOS.js").then(t=>t.ag),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
