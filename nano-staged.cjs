b1d4c378afdd28c18a0e7506eeb9c61310fecaea
white <294476387@qq.com>
2024-03-11 16:49:55 +0800

    build: update config

--- nano-staged.cjs
+++ nano-staged.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{vue,js,ts}': ['eslint --fix', 'prettier --write'],
   '*.{scss,css}': ['stylelint --fix', 'prettier --write'],
-  '*.{json,yml}': ['prettier --write'],
+  '*.{json,yml,yaml}': ['prettier --write'],
   '*.md': ['markdownlint', 'prettier --write'],
   'package.json': ['sort-package-json ./package.json'],
 };
